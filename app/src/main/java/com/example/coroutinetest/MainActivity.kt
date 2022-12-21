9dbdd1142fc203419af6a8408330b22d8f546810
kosov <kosov941@gmail.com>
2022-12-21 10:22:27 +0700

    Handler

---
+++ app/src/main/java/com/example/coroutinetest/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.coroutinetest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import androidx.core.view.isVisible
+import com.example.coroutinetest.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.buttonLoad.setOnClickListener {
+            loadData()
+        }
+    }
+
+    private fun loadData() {
+        binding.progress.isVisible = true
+        binding.buttonLoad.isEnabled = false
+        loadCity {
+            binding.tvLocation.text = it
+            loadTemperature(it) {
+                binding.tvTemperature.text = it.toString()
+                binding.progress.isVisible = false
+                binding.buttonLoad.isEnabled = true
+            }
+        }
+    }
+
+    private fun loadCity(callback: (String) -> Unit) {
+        thread {
+            Thread.sleep(5000)
+            Handler(Looper.getMainLooper()).post {
+                callback.invoke("Moscow")
+            }
+        }
+    }
+
+    private fun loadTemperature(city: String, callback: (Int) -> Unit) {
+        thread {
+            Handler(Looper.getMainLooper()).post {
+                Toast.makeText(
+                    this,
+                    getString(R.string.loading_temperature_toast, city),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            Thread.sleep(5000)
+            Handler(Looper.getMainLooper()).post {
+                callback.invoke(17)
+            }
+        }
+    }
+}
